55326147ff9ffe9861b3396f4f1087803d7bd036
Vinoth Kumar <vinoth.mani2@wipro.com>
2018-07-02 15:46:32 +0530

    FRP endpoint in Kotlin

--- reactive-web/src/main/kotlin/com/example/reactiveweb/ReactiveWebApplication.kt
+++ reactive-web/src/main/kotlin/com/example/reactiveweb/ReactiveWebApplication.kt
@@ -2,9 +2,23 @@ package com.example.reactiveweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Flux
 
 @SpringBootApplication
-class ReactiveWebApplication
+class ReactiveWebApplication {
+
+    @Bean
+    fun routes() = router {
+
+        GET("/frp/kotlin/hello") {
+            ServerResponse.ok().body(Flux.just("hello"), String::class.java)
+        }
+
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ReactiveWebApplication>(*args)
