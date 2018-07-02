927b30c8ce2b80a1eb2134d0f7810eb8942fb3b8
Vinoth Kumar <vinoth.mani2@wipro.com>
2018-07-02 15:51:22 +0530

    Initiliazing routers in SpringApplicationBuilder

--- reactive-web/src/main/kotlin/com/example/reactiveweb/ReactiveWebApplication.kt
+++ reactive-web/src/main/kotlin/com/example/reactiveweb/ReactiveWebApplication.kt
@@ -1,8 +1,10 @@
 package com.example.reactiveweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.support.beans
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Flux
@@ -21,5 +23,17 @@ class ReactiveWebApplication {
 }
 
 fun main(args: Array<String>) {
-    runApplication<ReactiveWebApplication>(*args)
+    SpringApplicationBuilder()
+            .sources(ReactiveWebApplication::class.java)
+            .initializers(beans {
+                bean {
+                    router {
+                        GET("/frp/kotlin/hi") {
+                            ServerResponse.ok().body(Flux.just("hi"), String::class.java)
+                        }
+                    }
+                }
+
+            })
+            .run(*args)
 }
